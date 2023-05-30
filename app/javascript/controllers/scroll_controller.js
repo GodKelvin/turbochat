37648cdaba5e84ccb01105cdbfa1426e2d78fb90
KelvinSRV <kelvin@seguroviagem.srv.br>
2023-05-30 17:39:03 -0300

    alterando css de login e home

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   /*On Start*/
   connect(){
-    console.log("connected");
     const messages = document.getElementById("messages");
     messages.addEventListener("DOMNodeInserted", this.resetScroll);
     this.resetScroll(messages);
@@ -11,11 +10,11 @@ export default class extends Controller {
   
   /* on stop*/
   disconnect(){
-    console.log("discconnected");
+
   }
 
   /*Custom function*/
   resetScroll(){
-    messages.scrollTop = messages.scrollHeight - messages.clientHeight
+    messages.scrollTop = messages.scrollHeight - messages.clientHeight;
   }
 }
